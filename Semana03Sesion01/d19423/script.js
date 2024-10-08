44a8e1a8097961e8f84691101aedb5b842551e49
Roberto Pineda <rpineda@x-codec.net>
2024-10-07 20:40:24 -0500

    Clase 07102024 primera parte

---
+++ Semana03Sesion01/d19423/script.js
@@ -0,0 +1,22 @@
+// alert("Hola desde el archivo script")
+
+Swal.fire({
+    title: "<strong>HTML <u>example</u></strong>",
+    icon: "info",
+    html: `
+      You can use <b>bold text</b>,
+      <a href="#" autofocus>links</a>,
+      and other HTML tags
+    `,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText: `
+      <i class="fa fa-thumbs-up"></i> Great!
+    `,
+    confirmButtonAriaLabel: "Thumbs up, great!",
+    cancelButtonText: `
+      <i class="fa fa-thumbs-down"></i>
+    `,
+    cancelButtonAriaLabel: "Thumbs down"
+  });
