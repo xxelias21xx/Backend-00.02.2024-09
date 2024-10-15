6acf68589c2bb127ef6801efbb6a32059c49f115
Luis Laos <SV46552638@idat.pe>
2024-10-14 22:04:12 -0500

    avance ejercicio 31

--- Hackaton03/SV46552638/script.js
+++ Hackaton03/SV46552638/script.js
@@ -421,3 +421,367 @@ function ejercicio18() {
     document.getElementById("e18_input1").value = '';
 	document.getElementById("Resultado_ejercicio18").innerHTML = `Total: ${total}   Comision:  ${comision} `;
 }
+
+function ejercicio19(){
+    /**
+     * 19. Hacer un algoritmo en JavaScript para una heladería se tienen 4 tipos de empleados ordenados 
+     * de la siguiente forma con su número identificador y salario diario correspondiente:
+
+        Cajero (56$/día).
+        Servidor (64$/día).
+        Preparador de mezclas (80$/día).
+        Mantenimiento (48$/día).
+
+        El dueño de la tienda desea tener un programa donde sólo ingrese dos números enteros que representen 
+        al número identificador del empleado y la cantidad de días que trabajó en la semana (6 días máximos).
+        Y el programa le mostrará por pantalla la cantidad de dinero que el dueño le debe pagar al empleado que ingresó.
+     */
+
+    // definir los empleados
+    let empleado1 = {
+        tipo: "Cajero",
+        tarifa: 56.0,
+        dias: 0.0,
+    };
+    let empleado2 = {
+        tipo: "Servidor",
+        tarifa: 64.0,
+        dias: 0.0,
+    };
+    let empleado3 = {
+        tipo: "Preparador de mezclas",
+        tarifa: 80.0,
+        dias: 0.0,
+    };
+    let empleado4 = {
+        tipo: "Mantenimiento ",
+        tarifa: 48.0,
+        dias: 0.0,
+    };
+
+    let empleados = [];
+    empleados.push(empleado1);
+    empleados.push(empleado2);
+    empleados.push(empleado3);
+    empleados.push(empleado4);
+    // console.log(empleados);
+
+    // let empleadosList = document.getElementById("empleadosList");
+    
+    // ingresar variables 
+    let idTrabajador =parseInt(document.getElementById("e19_input1").value);
+    let diasTrabajados = parseInt(document.getElementById("e19_input2").value);
+    let sueldoSemanal=0.0;
+
+    console.log(empleados)
+
+    if ((idTrabajador>0 && idTrabajador < 5)&&(diasTrabajados>0 && diasTrabajados < 7)){
+        
+        let sueldoSemanal=empleados[idTrabajador-1].tarifa * diasTrabajados;
+        document.getElementById("Resultado_ejercicio19").innerHTML = `ID: ${idTrabajador} -Tipo: ${empleados[idTrabajador-1].tipo} -Sueldo Semanal: ${sueldoSemanal}`;
+        document.getElementById("e19_input1").value = '';
+        document.getElementById("e19_input2").value = '';
+        
+    }else{
+        document.getElementById("e19_input1").value = '';
+        document.getElementById("e19_input2").value = '';
+        document.getElementById("Resultado_ejercicio19").innerHTML = `Valor erroneo en ID ó Dias trabajados`;
+    }
+}
+
+function ejercicio20() {
+    /*20. Hacer un algoritmo en JavaScript que que lea 4 números enteros positivos y verifique 
+        y realice las siguientes operaciones:
+
+        -¿Cuántos números son Pares?
+        -¿Cuál es el mayor de todos?
+        -Si el tercero es par, calcular el cuadrado del segundo.
+        -Si el primero es menor que el cuarto, calcular la media de los 4 números.
+        -Si el segundo es mayor que el tercero, verificar si el tercero esta comprendido entre los valores 50 y 700.
+        -Si cumple se cumple la segunda condición, calcular la suma de los 4 números.
+     * 
+     */
+    let numeros =[];
+    let elMayor=0;
+	let contadorPares=0;
+	let acumulador=0;
+
+    let num1 = parseInt(document.getElementById("e20_input1").value);
+    let num2 = parseInt(document.getElementById("e20_input2").value);
+    let num3 = parseInt(document.getElementById("e20_input3").value);
+    let num4 = parseInt(document.getElementById("e20_input4").value);
+    console.log(num1,num2,num3,num4);
+
+    if (num1 > 0 && num2 > 0 && num3 > 0 && num4 > 0) {
+        numeros.push(num1);
+        numeros.push(num2);
+        numeros.push(num3);
+        numeros.push(num4);
+        console.log(numeros);
+        acumulador = num1+num2+num3+num4;
+    } else {
+        document.getElementById("Resultado_ejercicio20").innerHTML = `Número incorrecto: `;
+        document.getElementById("e20_input1").value = '';
+        document.getElementById("e20_input2").value = '';
+        document.getElementById("e20_input3").value = '';
+        document.getElementById("e20_input4").value = '';
+    }
+
+    let respuesta1 = "";
+    let respuesta2 = "";
+    let respuesta3 = "";
+    let respuesta4 = "";
+    let respuesta5 = "";
+    let respuesta6 = "";
+    for (let index = 0; index < 4; index++) {
+        if (numeros[index] % 2 === 0) {
+            contadorPares++;
+            respuesta1= `Numeros pares :${contadorPares} `;
+        }
+        if (numeros[index] > elMayor) {
+            elMayor = numeros[index];
+            respuesta2= `El mayor es :${elMayor} `;
+        }
+    }
+
+    if (numeros[2] % 2 === 0) {
+        respuesta3= `2° al cuadrado: ${numeros[1]*numeros[1]}`;
+    }
+
+    if (numeros[0] < numeros[3]) {
+        respuesta4=`El Promedio es: ${acumulador/4}`;
+        respuesta6=`La sumatoria es: ${acumulador}`;
+    }
+    
+    if (numeros[1]>numeros[2]) {
+        if(numeros[2]>50 && numeros[2]<700){
+            respuesta5=`${numeros[2]}, comprendido entre 50 y 700`;
+        }
+    }
+     
+    document.getElementById("Resultado_ejercicio20").innerHTML = `
+    ${respuesta1},
+    ${respuesta2},
+    ${respuesta3},
+    ${respuesta4},
+    ${respuesta5},
+    ${respuesta6},
+    `;
+}
+
+function ejercicio21() {
+    //21. Hacer un algoritmo en JavaScript que permita calcular el factorial de un número.
+    let num = parseInt(document.getElementById("e21_input1").value);
+    let resultado=1;
+    for (let index = 1; index < num+1; index++) {
+        resultado=resultado*index;
+    }
+    document.getElementById("e21_input1").value = '';
+    document.getElementById("Resultado_ejercicio21").innerHTML=` ${num}! = ${resultado}`;
+}
+
+function ejercicio22() {
+    //22. Hacer un algoritmo en JavaScript para calcular la suma de los n primeros números.
+    let num=parseInt(document.getElementById("e22_input1").value);
+    let respuesta =((num)*(num+1))/2
+    document.getElementById("e22_input1").value = '';
+	document.getElementById("Resultado_ejercicio22").innerHTML=` Sumatoria = ${respuesta}`;
+}
+
+function ejercicio23() {
+    // Hacer un algoritmo en JavaScript para calcular la suma de los números impares menores o iguales a n.
+    let num=parseInt(document.getElementById("e23_input1").value);
+    let acumulador=0;
+    for (let index = 1; index < num+1; index++) {
+        if (index % 2 === 1 ) {
+            acumulador= acumulador+index;
+        }
+    }
+    document.getElementById("e23_input1").value = '';
+    document.getElementById("Resultado_ejercicio23").innerHTML=` Sumatoria = ${acumulador}`;
+}
+
+function ejercicio24(){
+    // 24. Hacer un algoritmo en JavaScript para realizar la suma de todos los números pares hasta el 1000.
+    let acumulador=0;
+    for (let index = 1; index < 501; index++) {
+        acumulador = acumulador+index;
+    }
+    document.getElementById("Resultado_ejercicio24").innerHTML=` Sumatoria = ${acumulador*2}`;
+
+}
+
+function ejercicio25() {
+    //25. Hacer un algoritmo en JavaScript para calcular el factorial de un número de una forma distinta
+    let num = parseInt(document.getElementById("e25_input1").value);
+    let resultado=1;
+    let i=1;
+
+    while (i < num+1 ) {
+        resultado*=i;
+        // console.log(resultado);
+		i++;
+    }
+    document.getElementById("e25_input1").value = '';
+    document.getElementById("Resultado_ejercicio25").innerHTML=` ${num}! = ${resultado}`;
+}
+
+function ejercicio26() {
+    // 26. Hacer un algoritmo en JavaScript para calcular el resto y cociente por medio de restas sucesivas.
+    let cociente=0;
+    let residuo=0;
+    let dividendo = parseFloat(document.getElementById("e26_input1").value);
+    const dvd = dividendo;
+    let divisor = parseFloat(document.getElementById("e26_input2").value);
+    const dvs = divisor;
+
+
+   while (dividendo > divisor) {
+    residuo=dividendo-divisor;
+    cociente ++;
+    dividendo=residuo
+   }
+   document.getElementById("e26_input1").value = '';
+   document.getElementById("e26_input2").value = '';
+   document.getElementById("Resultado_ejercicio26").innerHTML=`Dividendo: ${dvd} / Divisor: ${dvs} / Cociente: ${cociente} / Residuo: ${residuo}`;
+
+}
+
+let acumuladorE27=0.0;
+let contadorE27 =0;
+function ejercicio27(){
+    //27. Hacer un algoritmo en JavaScript para determinar la media de una lista indefinida de números positivos, se debe acabar el programa al ingresar un número negativo.
+    // let acumulador = 0.0;
+	let num = parseFloat(document.getElementById("e27_input1").value);
+    console.log(num);
+
+    if(contadorE27 === 0 && num === 0){
+        document.getElementById("Resultado_ejercicio27").innerHTML=`"El proceso no se puedo efectuar " `;
+    }else{
+        if(num > 0){
+            acumuladorE27=acumuladorE27+num;
+            document.getElementById("e27_input1").value = '';
+            contadorE27++;
+            document.getElementById("Resultado_ejercicio27").innerHTML=`"calculando  " `;
+        }else{
+            document.getElementById("e27_input1").value = '';
+            document.getElementById("Resultado_ejercicio27").innerHTML=`
+            "Acumulado: ${acumuladorE27}" / Catidad de elementos ${contadorE27} /
+            "El promedio es ": ${acumuladorE27/contadorE27} `;
+        }
+    }
+	
+}
+
+function ejercicio28(){
+    //28. Hacer un algoritmo en JavaScript para calcular la suma de los primeros cien números con un ciclo repetir.
+    let acumulador=0;
+    let indice=0;
+    do {
+        
+    } while (!(indice === 100));
+    document.getElementById("Resultado_ejercicio28").innerHTML=`Sumatoria : ${acumulador} `;
+}
+
+function ejercicio29(){
+    //29. Hacer un algoritmo en JavaScript para calcular la suma de los primeros cien números con un ciclo mientras.
+    let acumulador=0;
+    let indice=0;
+    
+    while (indice < 101) {
+		acumulador += indice;
+        indice ++;
+    }
+    document.getElementById("Resultado_ejercicio29").innerHTML=`Sumatoria : ${acumulador} `;     
+}
+
+function ejercicio30(){
+    //30. Hacer un algoritmo en JavaScript para calcular la suma de los primeros cien números con un ciclo para.
+    let acumulador=0;
+  
+    for (let indice = 1; indice < 101; indice++) {
+        acumulador += indice;
+    }
+    document.getElementById("Resultado_ejercicio30").innerHTML=`Sumatoria : ${acumulador} `; 
+}
+
+// let contadorE31=0;
+let acumuladorParesE31=0;
+let	contadorParesE31=0;
+let acumuladorImparesE31=0;
+let	contadorImparesE31=0;
+function ejercicio31(){
+    //31. Hacer un algoritmo en JavaScript parar calcular la media de los números pares e impares, sólo se ingresará diez números.    
+    let num = parseFloat(document.getElementById("e31_input1").value);
+    
+    if (num % 2 === 0) {
+        acumuladorParesE31 += num;
+        contadorParesE31 ++;
+        // console.log("conta", contadorParesE31,"acum", acumuladorParesE31);
+        document.getElementById("e31_input1").value = '';
+    } else {
+        acumuladorImparesE31 += num;
+        contadorImparesE31 ++;
+        // console.log("conta", contadorImparesE31,"acum", acumuladorImparesE31);
+        document.getElementById("e31_input1").value = '';
+    }
+    
+    if (contadorParesE31+contadorImparesE31 === 10) {
+        if (contadorParesE31 === 0) {
+            document.getElementById("e31_input1").value = '';
+            document.getElementById("Resultado_ejercicio31").innerHTML=`No hay pares y el promedio de impares: ${acumuladorImparesE31/contadorImparesE31}`;  
+        }else{
+            if (contadorImparesE31 === 0) {
+                document.getElementById("e31_input1").value = '';
+                document.getElementById("Resultado_ejercicio31").innerHTML=`No hay impares y el promedio de pares: ${acumuladorParesE31/contadorParesE31}`;  
+            }else{
+                document.getElementById("e31_input1").value = '';
+                document.getElementById("Resultado_ejercicio31").innerHTML=`promedio de pares ${acumuladorParesE31/contadorParesE31} y el promedio de impares: ${acumuladorImparesE31/contadorImparesE31}`;  
+            }
+        }
+    }
+    // console.log("iteracion ",contadorParesE31+contadorImparesE31)
+    // console.log("contaP", contadorParesE31,"acum", acumuladorParesE31);
+    // console.log("contaI", contadorImparesE31,"acum", acumuladorImparesE31);    
+
+}
+
+function ejercicio32(){
+    //32. Se quiere saber cuál es la ciudad con la población de más personas, son tres provincias y once ciudades, hacer un algoritmo en JavaScript que nos permita saber eso. 
+}
+
+function ejercicio33(){
+    //33. Hacer un algoritmo en JavaScript que permita al usuario continuar con el programa.
+}
+
+function ejercicio34(){
+    //34. Hacer un algoritmo en JavaScript que imprima la tabla de multiplicar de los números del uno al nueve.
+}
+
+function ejercicio35(){
+    //35. Hacer un algoritmo en JavaScript que nos permita saber cuál es el número mayor y menor, se debe ingresar sólo veinte números.
+}
+
+function ejercicio36(){
+    //36. Hacer un algoritmo en JavaScript para calcular la serie de Fibonacci.
+}
+
+function ejercicio37(){
+    //37. Hacer un algoritmo en JavaScript para conseguir el M.C.D de un número por medio del algoritmo de Euclides.
+}
+
+function ejercicio38(){
+    //38. Hacer un algoritmo en JavaScript que nos permita saber si un número es un número perfecto.    
+}
+
+function ejercicio39(){
+    /**39. Hacer un algoritmo en JavaScript que cumpla con la aproximación del número pi con la serie de Gregory-Leibniz. La formula que se debe aplicar es:
+        Pi = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) .*/
+    
+}
+
+function ejercicio40(){
+    //40. Hacer un algoritmo en JavaScript que cumpla con la aproximación del número pi con la serie de Nilakantha. La formula que se debe aplicar es:
+    //Pi = 3 + 4/(2*3*4) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10) + 4/(10*11*12) - 4/(12*13*14) ...
+    
+}
