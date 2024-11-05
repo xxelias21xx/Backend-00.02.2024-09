9e8d19db5ed5ab704a0bd63bbfd5407d369132ce
Roberto Pineda Lopez <rpineda@MacBook-Pro-de-Roberto.local>
2024-11-04 21:31:35 -0500

    Clase 04112024 segunda parte

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,6 +1,8 @@
 let server = require("http");
 let port = 3000;
+var moment = require('moment'); // require
 
 server.createServer(function(req, res){
-res.end("Hola desde mi app en nodejs")
+    console.log(moment().format())
+    res.end("Hola desde mi app en nodejs "+ moment().format("[Today is] dddd") )
 }).listen(port);
