a450902e71fa8c51a38775377513614fa600fe78
carlosusuriaga <carlosusda@gmail.com>
2024-11-04 21:34:01 -0500

    regularizacion de fuentes

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,6 +1,10 @@
+//Esta línea importa el módulo http
 let server = require("http");
+//definimos el puerto que vamos a usar
 let port = 3000;
 
-server.createServer(function(req, res){
+//creamos un servidor con el http y usamos createserver()
+server.createServer(function(req, res){//le pasamos una funcion para que nos de una resuesta
 res.end("Hola desde mi app en nodejs")
-}).listen(port);
+}).listen(port); // aqui usamos listen() para decirle que el servidor usara o escuchara el puerto 3000
+//pasamos la varaible port
