67fb4d488dc1feb2cff4225c1837571338c3816e
Roberto Pineda Lopez <rpineda@MacBook-Pro-de-Roberto.local>
2024-11-06 21:12:11 -0500

    Clase 06112024 primera parte

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,8 +1,32 @@
 let server = require("http");
 let port = 3000;
 var moment = require('moment'); // require
+let fecha = require('./miModulo')
+let fs = require('fs');
+var url = require('url');
 
 server.createServer(function(req, res){
-    console.log(moment().format())
-    res.end("Hola desde mi app en nodejs "+ moment().format("[Today is] dddd") )
+    // console.log(moment().format())
+    // console.log(req.url);
+
+
+    if(req.url === "/clima"){
+        res.end("Esta haciendo frio");
+    }
+    if(req.url=== "/tv"){
+        fs.readFile("tv.html",function(error, data){
+            res.write(data);
+            res.end();
+        })
+    }
+    if(req.url === "/pregunta/"){
+        var q = url.parse(req.url, true);
+        console.log(q)
+        res.end();
+    }
+    else{
+        res.writeHead(200, {'Content-Type': 'text/text'});
+        res.end("<h1>Hola desde mi app en nodejs "+ moment().format("[Today is] dddd") + " " + fecha.miFecha()+"</h1>" )
+    }
+
 }).listen(port);
