1ef71412d7f0fd35f219dfe1ff69932c27391553
Roberto Pineda Lopez <rpineda@MacBook-Pro-de-Roberto.local>
2024-11-18 20:49:41 -0500

    Clase 18112024 primera parte

--- Semana09Sesion01/d19423/DDL.sql
+++ Semana09Sesion01/d19423/DDL.sql
@@ -1,4 +1,5 @@
 drop table if exists sexo;
+drop table if exists raza;
 drop table if exists usuario;
 
 
@@ -43,4 +44,22 @@ values('Macho',1)
 ,('Hembra', 1);
 
 select * from sexo;
+
+create table raza(
+	id serial primary key,
+	descripcion varchar(100) not null unique,
+	is_activo bit default '1'::bit not null,
+	usuario_creacion int not null,
+	fecha_creacion timestamp not null default now(),
+	usuario_modificacion int null,
+	fecha_modificacion timestamp null,
+	foreign key (usuario_creacion) references usuario(id),
+	foreign key (usuario_modificacion) references usuario(id)
+);
+
+insert into raza (descripcion, usuario_creacion)
+values('Mestizo',1)
+,('Azul Ruso', 1);
+
+select * from raza;
 
