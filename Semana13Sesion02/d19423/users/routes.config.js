a2f35b7ab8b427292b3de3c38af01a162649582a
Roberto Pineda Lopez <rpineda@x-codec.net>
2024-12-18 20:36:42 -0500

    Clase 18122024 segunda parte

--- Semana13Sesion02/d19423/users/routes.config.js
+++ Semana13Sesion02/d19423/users/routes.config.js
@@ -1,5 +1,10 @@
+const UsersControllers = require('./controllers/users.controller');
+
 exports.routesConfig = function (app) {
-    app.get("/users",(req,res)=>{
-        res.send({message:"Hola desde users"});
-    })
+    // app.get("/users",(req,res)=>{
+    //     res.send({message:"Hola desde users"});
+    // })
+    app.post('/users',[
+        UsersControllers.insert
+    ]);
 }
