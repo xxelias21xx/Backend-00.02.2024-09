c6fd6e71728b31ec162ed7390dcf682077f9944b
Roberto Pineda Lopez <rpineda@x-codec.net>
2024-12-18 22:04:48 -0500

    Clase 18122024 tercera parte

--- Semana13Sesion02/d19423/users/routes.config.js
+++ Semana13Sesion02/d19423/users/routes.config.js
@@ -1,4 +1,11 @@
 const UsersControllers = require('./controllers/users.controller');
+const PermissionMiddleware = require('../common/middlewares/auth.permission.middleware');
+const ValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
+const config = require('../common/config/env.config');
+
+const ADMIN = config.permissionLevels.ADMIN;
+const PAID = config.permissionLevels.PAID_USER;
+const FREE = config.permissionLevels.NORMAL_USER;
 
 exports.routesConfig = function (app) {
     // app.get("/users",(req,res)=>{
@@ -6,5 +13,27 @@ exports.routesConfig = function (app) {
     // })
     app.post('/users',[
         UsersControllers.insert
+    ]);
+    app.get('/users', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(PAID),
+        UsersControllers.list
+    ]);
+    app.get('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersControllers.getById
+    ]);
+    app.patch('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(FREE),
+        PermissionMiddleware.onlySameUserOrAdminCanDoThisAction,
+        UsersControllers.patchById
+    ]);
+    app.delete('/users/:userId', [
+        ValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
+        UsersControllers.removeById
     ]);
 }
