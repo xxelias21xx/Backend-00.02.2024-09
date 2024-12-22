92ebeb2da9d7653f6f4238e887f4ad57a7397d9f
Fabian <saltamontehz@gmail.com>
2024-12-21 19:21:47 -0500

    Hackaton 12 Completa

--- Hackaton12/SV71286722/app.mjs
+++ Hackaton12/SV71286722/app.mjs
@@ -1,3 +1,60 @@
 import http from 'node:http'
 import url from 'node:url'
 
+let listSales = [];
+const server = http.createServer((req, res) => {
+ 
+    const parseUrl = url.parse(req.url, true);
+    const path = parseUrl.pathname;
+
+    //localhost:3000/api/products -- get
+    // endpoint: lists
+    if (req.method == "GET" && path === "/api/lista") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify(listSales));
+    }
+    // Ruta para mostrar los elementos pendientes
+    if (req.method == "GET" && path === "/api/lista/pendientes") {
+        const pendientes = listSales.filter(item => !item.esCompletado);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify(pendientes));
+    }
+
+    // Ruta para mostrar los elementos completados
+    if (req.method == "GET" && path === "/api/shopping-list/completed") {
+        const completados = listSales.filter(item => item.esCompletado);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.write(JSON.stringify(completados));
+    }
+
+    // endpoint: crear elemento de lista
+    if (req.method == "POST" && path == "/api/lista") {
+        console.log("creating a new sale");
+
+        let body = "";
+        req.on("data", (chunk) => {
+                body += chunk.toString();
+        });
+
+        return req.on("end", () => {
+            const { name, description, date, esCompletado  } = JSON.parse(body);
+
+            if (!name || !description || !date || !esCompletado) {
+                res.writeHead(400, { "content-type": "apllication/json" });
+                return res.end(JSON.stringify({ message: "faltan campos" }));
+            }
+
+            listSales.push(data);
+
+            res.writeHead(201, { "content-type": "apllication/json" });
+            return res.end(JSON.stringify(listSales));
+        });
+    }
+
+    res.writeHead(503, { "content-type": "apllication/json" });
+    res.end(JSON.stringify({ message: "endpoint not found" }));
+});
+ 
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
