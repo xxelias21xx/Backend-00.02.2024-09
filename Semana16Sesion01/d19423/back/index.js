0cf908bfebafb211144ed9b15c0a68df769fb4b0
Roberto Pineda Lopez <rpineda@x-codec.net>
2025-01-20 22:07:19 -0500

    Clase 20012025 segunda parte

--- Semana16Sesion01/d19423/back/index.js
+++ Semana16Sesion01/d19423/back/index.js
@@ -12,7 +12,30 @@ app.use(cors());
 app.get('/', (req, res)=>{
     res.send("Hola desde el back");
 })
+app.post("/api/create-checkout-session",async (req,res)=>{
+    const {product} = req.body;
+    console.log(req.body)
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:[
+            {
+                price_data: {
+                    currency: "pen",
+                    product_data: {
+                        name: product.name
+                    },
+                    unit_amount: product.price * 100
+                },
+                quantity: product.quantity
+            }
+        ],
+        mode: "payment",
+        success_url: 'http://localhost:3000/success',
+        cancel_url: "http://localhost:3000/cancel"
 
+    });
+    res.json({id: session.id});
+})
 app.listen(8000,()=>{
     console.log("Servidor iniciado en el puerto 8000")
 })
