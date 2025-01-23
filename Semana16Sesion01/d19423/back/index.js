a9bdf84f16bb08594603028c0de2381ef5c6f1b2
Luis Laos <llaos.dev@gmail.com>
2025-01-22 19:29:36 -0500

    semana 16 sesion 1

--- Semana16Sesion01/d19423/back/index.js
+++ Semana16Sesion01/d19423/back/index.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-
+ 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); 
 
 app.get('/', (req, res)=>{
     res.send("Hola desde el back");
